4cee98b578c949350347b6007cb0e452bb311314
Harshal Holkar <93507639+Harshal-Holkar@users.noreply.github.com>
2022-06-11 10:55:54 +0530

    Delete Landing page with Responsive menu directory

--- Landing page with Responsive menu/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".burger").on("click", function() {
-        $(".wrapper nav ul").slideToggle(500);
-    });
-});
-
-function animateBar(x) {
-    x.classList.toggle("change");
-}
